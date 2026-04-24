3c6385b94cf439c4f95910e492ac262b35ca0a27
haracedaily <nj7001@naver.com>
2026-04-24 17:21:35 +0900

    [NEW} 별찍기

---
+++ JAVASCRIPT/1.javascript.js
@@ -0,0 +1,10 @@
+for(let i=5;i>0;i--){console.log("*".repeat(i));}
+
+for(let i=1;i<6;i++){console.log(" ".repeat(5-i)+"*".repeat(i))}
+
+for(let i=0;i<5;i++){console.log(" ".repeat(i)+"*".repeat(5-i))}
+
+for(let i=0;i<6;i++){console.log(" ".repeat(5-i)+"*".repeat(i*2+1))}
+
+for(let i=0;i<6;i++){console.log(" ".repeat(5-i)+"*".repeat(i*2+1))}
+for(let i=1;i<6;i++){console.log(" ".repeat(i)+"*".repeat((5-i)*2+1))}
